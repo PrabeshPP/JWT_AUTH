ec493940840151d8bbf466ef7a8e455461c5f4e8
PrabeshPP <prabesh.bista369@gmail.com>
2023-03-15 18:26:16 -0400

    [Completed] JWT in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import Cookies from "js-cookie";
 import { Route, Routes } from "react-router";
 import NavBar from "./components/navBar";
 import Login from "./components/page/login";
 import NormalPage from "./components/page/normalPage";
 import PrivateRoutes from "./components/page/protectedRoutes";
 import SecretPage from "./components/page/secretPage";
-
+import { setAuthToken } from "./useAuthToken";
 
 function App() {
+  const token=Cookies.get("jwt");
+  if(token){
+    setAuthToken(token);
+  }
   return (
     <div className=" min-h-[100vh] w-[100%]">
         <NavBar/>
